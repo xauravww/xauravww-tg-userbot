e000954aae9256ae42bdd597f67607d628e4ad2d
xauravww <sauravmaheshwari8@gmail.com>
2024-02-22 16:43:20 +0530

    add error checkng for server

--- msgs.js
+++ msgs.js
@@ -2,7 +2,8 @@ const { NewMessage } = require("telegram/events")
 require("./client")
 const { client, connectClient } = require("./client")
 const { eventPrint } = require("./controllers/msgs")
-
+const express = require("express")
+const app = express()
 ;(async function run() {
   connectClient()
 
@@ -15,3 +16,11 @@ const { eventPrint } = require("./controllers/msgs")
   // Keep the program running indefinitely
   await new Promise(() => {})
 })()
+
+app.get("/", (req, res) => {
+  res.send("I am working properly")
+})
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000")
+})
