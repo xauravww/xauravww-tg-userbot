5d06e9ee78069e88d9c5ed360fcd4853d6554ea6
xauravww <sauravmaheshwari8@gmail.com>
2024-02-23 00:53:27 +0530

    fixed gifs in dms

--- client.js
+++ client.js
@@ -1,20 +1,31 @@
 const { StringSession } = require("telegram/sessions")
 const { Api, TelegramClient } = require("telegram")
+const path = require("path")
 const dotenv = require("dotenv")
-dotenv.config({ path: "./env" })
+dotenv.config({ path: path.resolve(".env") })
 
-const apiId = parseInt(process.env.API_KEY || "123456")
-const apiHash = process.env.API_HASH || "abcdefghijklmnopqrstuvwxyz"
+const apiId = parseInt(process.env.API_KEY)
+const apiHash = process.env.API_HASH
 
 const session = new StringSession(process.env.SESSION_STRING) // You should put your string session here
 
 const client = new TelegramClient(session, apiId, apiHash, {})
-
+console.log(client)
+console.log("API KEY is " + process.env.API_KEY)
+console.log("API Hash is " + process.env.API_HASH)
+console.log("Session is " + process.env.SESSION_STRING)
 // Connect the client once at the start of your application
 client
   .connect()
-  .then(() => {
-    console.log("Connected to Telegram")
+  .then((data) => {
+    console.log(data)
+    console.log(client.session.save())
+
+    if (client.session.save() == process.env.SESSION_STRING) {
+      console.log("Both are same")
+    } else {
+      console.log("Both are different")
+    }
   })
   .catch((err) => {
     console.error("Error connecting to Telegram:", err)
@@ -25,6 +36,7 @@ async function connectClient() {
     await client.connect()
     console.log("Connected to Telegram")
   }
+  // console.log(client.session.save())
 }
 
 module.exports = { client, connectClient }
