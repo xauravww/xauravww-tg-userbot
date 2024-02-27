bde19a544a6dd60157ad936ea139fe04e3c134b6
xauravww <sauravmaheshwari8@gmail.com>
2024-02-27 22:22:41 +0530

    Fixed: New ping

--- ping.js
+++ ping.js
@@ -1,15 +1,38 @@
 import { createRequire } from "module"
 const require = createRequire(import.meta.url)
 
-const { exec } = require("child_process")
-exec("ping -c 10 google.com", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`)
-    return
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`)
-    return
-  }
-  console.log(`stdout: ${stdout}`)
-})
+const { spawn } = require("child_process")
+
+function getPingTime() {
+  return new Promise((resolve, reject) => {
+    const command = "ping"
+    const args = ["www.google.com"]
+
+    const pingProcess = spawn(command, args)
+
+    let avgPingTime
+
+    pingProcess.stdout.on("data", (data) => {
+      const output = data.toString()
+      const matches = output.match(/Average = (\d+)ms/)
+      if (matches && matches.length > 1) {
+        avgPingTime = parseInt(matches[1])
+        console.log(`Average ping time: ${avgPingTime}ms`)
+        resolve(avgPingTime)
+      }
+    })
+
+    pingProcess.stderr.on("data", (data) => {
+      console.error(`stderr: ${data}`)
+      reject(data.toString())
+    })
+
+    pingProcess.on("close", (code) => {
+      console.log(`child process exited with code ${code}`)
+    })
+  })
+}
+
+// getPingTime()
+
+export { getPingTime }
