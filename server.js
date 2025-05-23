a8a8a53a3ddd24f8f465dbe7d709433c56d0441a
sauravtechno <sauravmaheshwari8@gmail.com>
2025-05-23 21:41:59 +0530

    Add Let's Encrypt challenge route

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ app.get("/", (req, res) => {
   res.send("I am working properly");
 });
 
+app.use('/.well-known/acme-challenge', express.static(path.join(__dirname, 'public/.well-known/acme-challenge')));
+
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
